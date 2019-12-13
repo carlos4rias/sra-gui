6d703165d483c9ba93385c01db2c4eea621c4424
carlos4rias <carlos.23325@gmail.com>
2019-12-13 00:03:07 -0500

    Adds buttons funtionality for call other interfaces.

--- main.py
+++ main.py
@@ -13,14 +13,54 @@ class Main( tk.Frame ):
 
 
         # Training button
-        training_btn = tk.Button( frame, text = "Entrenar", command = self.Run, state = tk.DISABLED )
-        mark_btn = tk.Button( frame, text = "Marcar Imagenes", command = self.Run )
-        run_btn = tk.Button( frame, text = "Ejecutar", command = self.Run )
-        settings_btn = tk.Button( frame, text = "Configuracion", command = self.Run )
+        training_btn = tk.Button(
+            frame,
+            text = "Entrenar",
+            command = lambda : self.Run('Training'),
+            state = tk.DISABLED
+        )
+
+        # Mark button
+        mark_btn = tk.Button(
+            frame,
+            text = "Marcar Imagenes",
+            command = lambda : self.Run('YoloMark'),
+            state = tk.DISABLED
+        )
+
+        # Run button
+        run_btn = tk.Button(
+            frame,
+            text = "Ejecutar",
+            command = lambda : self.Run('Run'),
+            state = tk.DISABLED
+        )
+
+        # Setting button
+        settings_btn = tk.Button(
+            frame,
+            text = "Configuracion",
+            command = lambda : self.Run('Setting'),
+        )
+
+        left_config_lbl = tk.Label(frame)
+        
+        show_message = True
 
         if 'darknet' in config.paths:
-            training_btn.state = tk.ACTIVE
+            training_btn["state"] = tk.NORMAL
+            run_btn["state"] = tk.NORMAL
+            show_message = False
+        
+        if 'yolomark' in config.paths:
+            mark_btn["state"] = tk.NORMAL
+            show_message = False
 
+        if show_message:
+            left_config_lbl["text"] = "Aun no se han seteado algunas rutas, configuralas en la opcion configuracion"
+            left_config_lbl.grid( row = 4, column = 0, sticky="nsew", pady = 15)
+
+    
         # Show widgets
         frame.grid( row = 0, column = 0, padx = 20, pady = 20)
         
@@ -30,5 +70,5 @@ class Main( tk.Frame ):
         settings_btn.grid( row = 3, column = 0, sticky="nsew")
 
 
-    def Run(self):
-        print("HOLA")
+    def Run(self, window):
+        self.controller.show_frame(window)
